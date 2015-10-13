ec42bec01ebd2a1ef1eb8d4b336d06395f15e706
deninson18 <deninson050895@gmail.com>
2015-10-13 15:55:22 -0430

    agregando ejercicio del cap 11 y 12

---
+++ Ejercicio11_12/Capitulo11_12.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Ejercicio11_12
+{
+    partial class Capitulo11_12
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.capitulo11ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejercicio1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejercicio2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejercicio3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejercicio4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejercicio5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.capitulo12ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejercicio1ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejercicio2ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejercicio3ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejercicico4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejercicio5ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.capitulo11ToolStripMenuItem,
+            this.capitulo12ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // capitulo11ToolStripMenuItem
+            // 
+            this.capitulo11ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ejercicio1ToolStripMenuItem,
+            this.ejercicio2ToolStripMenuItem,
+            this.ejercicio3ToolStripMenuItem,
+            this.ejercicio4ToolStripMenuItem,
+            this.ejercicio5ToolStripMenuItem});
+            this.capitulo11ToolStripMenuItem.Name = "capitulo11ToolStripMenuItem";
+            this.capitulo11ToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.capitulo11ToolStripMenuItem.Text = "Capitulo 11";
+            // 
+            // ejercicio1ToolStripMenuItem
+            // 
+            this.ejercicio1ToolStripMenuItem.Name = "ejercicio1ToolStripMenuItem";
+            this.ejercicio1ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ejercicio1ToolStripMenuItem.Text = "ejercicio 1";
+            this.ejercicio1ToolStripMenuItem.Click += new System.EventHandler(this.ejercicio1ToolStripMenuItem_Click);
+            // 
+            // ejercicio2ToolStripMenuItem
+            // 
+            this.ejercicio2ToolStripMenuItem.Name = "ejercicio2ToolStripMenuItem";
+            this.ejercicio2ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ejercicio2ToolStripMenuItem.Text = "ejercicio 2";
+            // 
+            // ejercicio3ToolStripMenuItem
+            // 
+            this.ejercicio3ToolStripMenuItem.Name = "ejercicio3ToolStripMenuItem";
+            this.ejercicio3ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ejercicio3ToolStripMenuItem.Text = "ejercicio 3";
+            // 
+            // ejercicio4ToolStripMenuItem
+            // 
+            this.ejercicio4ToolStripMenuItem.Name = "ejercicio4ToolStripMenuItem";
+            this.ejercicio4ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ejercicio4ToolStripMenuItem.Text = "ejercicio 4";
+            // 
+            // ejercicio5ToolStripMenuItem
+            // 
+            this.ejercicio5ToolStripMenuItem.Name = "ejercicio5ToolStripMenuItem";
+            this.ejercicio5ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ejercicio5ToolStripMenuItem.Text = "ejercicio 5";
+            // 
+            // capitulo12ToolStripMenuItem
+            // 
+            this.capitulo12ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ejercicio1ToolStripMenuItem1,
+            this.ejercicio2ToolStripMenuItem1,
+            this.ejercicio3ToolStripMenuItem1,
+            this.ejercicico4ToolStripMenuItem,
+            this.ejercicio5ToolStripMenuItem1});
+            this.capitulo12ToolStripMenuItem.Name = "capitulo12ToolStripMenuItem";
+            this.capitulo12ToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.capitulo12ToolStripMenuItem.Text = "Capitulo 12";
+            // 
+            // ejercicio1ToolStripMenuItem1
+            // 
+            this.ejercicio1ToolStripMenuItem1.Name = "ejercicio1ToolStripMenuItem1";
+            this.ejercicio1ToolStripMenuItem1.Size = new System.Drawing.Size(127, 22);
+            this.ejercicio1ToolStripMenuItem1.Text = "ejercicio 1";
+            // 
+            // ejercicio2ToolStripMenuItem1
+            // 
+            this.ejercicio2ToolStripMenuItem1.Name = "ejercicio2ToolStripMenuItem1";
+            this.ejercicio2ToolStripMenuItem1.Size = new System.Drawing.Size(127, 22);
+            this.ejercicio2ToolStripMenuItem1.Text = "ejercicio 2";
+            // 
+            // ejercicio3ToolStripMenuItem1
+            // 
+            this.ejercicio3ToolStripMenuItem1.Name = "ejercicio3ToolStripMenuItem1";
+            this.ejercicio3ToolStripMenuItem1.Size = new System.Drawing.Size(127, 22);
+            this.ejercicio3ToolStripMenuItem1.Text = "ejercicio 3";
+            // 
+            // ejercicico4ToolStripMenuItem
+            // 
+            this.ejercicico4ToolStripMenuItem.Name = "ejercicico4ToolStripMenuItem";
+            this.ejercicico4ToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.ejercicico4ToolStripMenuItem.Text = "ejercicio 4";
+            this.ejercicico4ToolStripMenuItem.Click += new System.EventHandler(this.ejercicico4ToolStripMenuItem_Click);
+            // 
+            // ejercicio5ToolStripMenuItem1
+            // 
+            this.ejercicio5ToolStripMenuItem1.Name = "ejercicio5ToolStripMenuItem1";
+            this.ejercicio5ToolStripMenuItem1.Size = new System.Drawing.Size(127, 22);
+            this.ejercicio5ToolStripMenuItem1.Text = "ejercicio 5";
+            // 
+            // Capitulo11_12
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Capitulo11_12";
+            this.Text = "Capitulo 11_12";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem capitulo11ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ejercicio1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ejercicio2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ejercicio3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ejercicio4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ejercicio5ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem capitulo12ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ejercicio1ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ejercicio2ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ejercicio3ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem ejercicico4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ejercicio5ToolStripMenuItem1;
+    }
+}
+
---
+++ Ejercicio11_12/Capitulo11_12.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio11_12
+{
+    public partial class Capitulo11_12 : Form
+    {
+        public Capitulo11_12()
+        {
+            InitializeComponent();
+        }
+
+        private void ejercicico4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ejercicio1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ejercicio11_1 ej = new ejercicio11_1();
+            ej.Show();
+        }
+    }
+}
